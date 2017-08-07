199e1161b9bed9d6bd7b0cb057a006ebda4123f1
yasinasama <yasinasama01@gmail.com>
2017-08-08 00:05:20 +0800

    change

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Crawl the images from Pixiv with Python
 碰到的问题：
   1. 登录需要post_key，post_key在登录页面源码中能找到，所以需要一开始请求一次登录页面。
   2. p站大图存在防盗链，需要在headers中加入Referer：url(点击进入图片的那个URL)
+  3.多图链接我按照单图的URL转换也能抓取
--- py_pixiv.py
+++ py_pixiv.py
@@ -2,10 +2,11 @@ import requests
 from bs4 import BeautifulSoup
 import re
 import os
+from datetime import datetime
 
 
-def getRequest(req, url, headers, stream=False):
-    return req.get(url=url, headers=headers, stream=stream) if req else requests.get(url=url, headers=headers, stream=stream)
+CRAWL_Url = 'https://www.pixiv.net/ranking.php?mode=monthly'  # 需要抓取的URL
+DOWNLOAD_PATH = 'D:\pixiv'   # 图片存放地址
 
 
 headers = {
@@ -24,51 +25,77 @@ data = {
     'post_key': ''
 }
 
-# 需要抓取的URL
-Crawl_Url = 'https://www.pixiv.net/ranking.php?mode=monthly'
 
-# 获取session实例
-p = requests.Session()
+def downloadPath(path):
+    if not os.path.exists(path):
+        os.mkdir(path)
+    return path
 
-# 获取post请求必要数据
-p.headers = headers
-r = getRequest(req=p, url='https://accounts.pixiv.net/login', headers=headers)
-parrern = re.compile(r'name="post_key" value="(.*?)">')
-res = parrern.search(r.text)
-data['post_key'] = res.group()
 
-# post请求模拟登陆
-t = p.post(url='https://accounts.pixiv.net/api/login', data=data)
-result = getRequest(req=p, url=Crawl_Url, headers=headers)
+def getRequest(req, url, headers, stream=False):
+    return req.get(url=url, headers=headers, stream=stream) if req else requests.get(url=url, headers=headers, stream=stream)
+
+
+def loginPixiv():
+    # 获取session实例
+    p = requests.Session()
+    # 获取post请求必要数据
+    p.headers = headers
+    r = getRequest(req=p, url='https://accounts.pixiv.net/login', headers=headers)
+    parrern = re.compile(r'name="post_key" value="(.*?)">')
+    res = parrern.search(r.text)
+    data['post_key'] = res.group()
+
+    # post请求模拟登陆
+    print('模拟登陆开始......')
+    try:
+        p.post(url='https://accounts.pixiv.net/api/login', data=data)
+        print('模拟登陆成功......')
+        return p
+    except:
+        print('模拟登陆失败......')
+        raise
+
+login = loginPixiv()
+result = getRequest(req=login, url=CRAWL_Url, headers=headers)
 
 # 获取我们分析的主要的DOM节点信息
 soup = BeautifulSoup(result.text, "html.parser")
 items = soup.find_all(class_='ranking-item')
 items_length = len(items)
 
-re_download_name = re.compile('(\d+)')
+re_filename = re.compile('(\d+)')
+downloadpath = downloadPath(DOWNLOAD_PATH)
 
-for i in range(5):
+for i in range(10):
     image_url = 'https://www.pixiv.net' + items[i].find(class_='ranking-image-item').a.get('href')
-
-    small_jpg_url = items[i].find('img').get('data-src')
-
-    large_jpg_url = small_jpg_url.replace(r'c/240x480/img-master', 'img-original').replace('_master1200', '')
-    large_png_url = large_jpg_url.replace('jpg', 'png')
-
-    download_name = re.search(re_download_name, image_url).group()+'.jpg'
-
     headers['Referer'] = image_url
+    filename = re.search(re_filename, image_url).group() + '.jpg'
+    fullpath = os.path.join(downloadpath, filename)
 
-    jpg = getRequest(req=p, url=large_jpg_url, headers=headers, stream=True)
-    png = getRequest(req=p, url=large_png_url, headers=headers, stream=True)
-    with open(download_name, 'wb') as f:
+    page_count = items[i].find(class_='page-count')
+    if page_count:
+
+    else:
+        small_jpg_url = items[i].find('img').get('data-src')
+
+        large_jpg_url = small_jpg_url.replace(r'c/240x480/img-master', 'img-original').replace('_master1200', '')
+        print(large_jpg_url)
+        large_png_url = large_jpg_url.replace('jpg', 'png')
+
+        jpg = getRequest(req=login, url=large_jpg_url, headers=headers, stream=True)
+        png = getRequest(req=login, url=large_png_url, headers=headers, stream=True)
+    with open(fullpath, 'wb') as f:
         if jpg.status_code == 200:
-            print('get it JPG!')
+            print('抓取开始%s' % filename)
+            s = datetime.now()
             f.write(jpg.content)
+            print('抓取完毕 , 耗时 %s 秒 , 文件大小 %s KB' % ((datetime.now()-s).seconds, int(os.path.getsize(fullpath)/1024)))
         elif png.status_code == 200:
-            print('get it PNG!')
+            print('抓取开始%s' % filename)
+            s = datetime.now()
             f.write(png.content)
+            print('抓取完毕 , 耗时 %s 秒 , 文件大小 %s KB' % ((datetime.now() - s).seconds, int(os.path.getsize(fullpath)/1024)))
         else:
             print('NOT FOUND')
 
