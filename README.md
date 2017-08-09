95ac1473ae7e0fad81adf144beac80921f7f6a4a
yasinasama <yasinasama01@gmail.com>
2017-08-09 17:13:59 +0800

    change

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Crawl the images from Pixiv with Python
   1. 登录需要post_key，post_key在登录页面源码中能找到，所以需要一开始请求一次登录页面。
   2. p站大图存在防盗链，需要在headers中加入Referer：url(点击进入图片的那个URL)
   3.多图链接我按照单图的URL转换也能抓取
+  4. 排行榜最多500张  也就是说param P<=10
--- py_pixiv.py
+++ py_pixiv.py
@@ -4,19 +4,21 @@ import re
 import os
 from datetime import datetime
 
-CRAWL_Url = 'https://www.pixiv.net/ranking.php?mode=monthly'  # 需要抓取的URL
+MAX_RANK_PAGE = 10
+CRAWL_Url = 'https://www.pixiv.net/ranking.php?mode=daily&p=1'  # 需要抓取的URL
 DOWNLOAD_PATH = 'D:\pixiv'  # 图片存放地址
 
 re_filename = re.compile('(\d+)')
 
+# 防盗链
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.109 Safari/537.36',
     'Referer': ''
 }
-
+# post请求内容
 data = {
-    'pixiv_id': '614867000@qq.com',
-    'password': 'yasina',
+    'pixiv_id': '',
+    'password': '',
     'captcha': '',
     'g_recaptcha_response': '',
     'source': 'pc',
@@ -32,19 +34,23 @@ def downloadPath(path):
     return path
 
 
-def getRequest(req, url, headers, stream=False):
-    return req.get(url=url, headers=headers, stream=stream) if req else requests.get(url=url, headers=headers, stream=stream)
+def isImageExist(filename):
+    return True if os.path.exists(filename) else False
 
 
 def loginPixiv():
+    pixiv_id = input('请输入账号: ')
+    password = input('请输入密码: ')
     # 获取session实例
     p = requests.Session()
     # 获取post请求必要数据
     p.headers = headers
-    r = getRequest(req=p, url='https://accounts.pixiv.net/login', headers=headers)
+    r = p.get(url='https://accounts.pixiv.net/login', headers=headers)
     parrern = re.compile(r'name="post_key" value="(.*?)">')
     res = parrern.search(r.text)
     data['post_key'] = res.group()
+    data['pixiv_id'] = pixiv_id
+    data['password'] = password
 
     # post请求模拟登陆
     print('模拟登陆开始......')
@@ -62,51 +68,53 @@ def isSingleImage(imgdom):
 
 
 # 单张图片抓取
-def singleImageCrawl(small_jpg_url, filename):
+def singleImageCrawl(small_jpg_url, filename, login_req):
     downloadpath = downloadPath(DOWNLOAD_PATH)
-    large_jpg_url = small_jpg_url.replace('c/240x480/img-master', 'img-original').replace('_master1200', '')
+    large_jpg_url = small_jpg_url.replace(r'c/240x480/img-master', 'img-original').replace('_master1200', '')
     large_png_url = large_jpg_url.replace('jpg', 'png')
     print(large_jpg_url)
-    downloadImage(downloadpath, filename, large_jpg_url, large_png_url)
+    downloadImage(downloadpath, filename, large_jpg_url, large_png_url, login_req)
 
 
 # 多张图片抓取
-def manyImageCrawl(small_jpg_url, filename, imgdom):
+def manyImageCrawl(small_jpg_url, filename, imgdom, login_req):
     downloadpath = downloadPath(DOWNLOAD_PATH)
     page_count = imgdom.find(class_='page-count').span.text
     for j in range(int(page_count)):
         f = filename.replace('.', '-' + str(j) + '.')
         p = '_p' + str(j) + '_'
-        large_jpg_url = small_jpg_url.replace('c/240x480/', '').replace('_p0_', p)
+        large_jpg_url = small_jpg_url.replace(r'c/240x480/', '').replace('_p0_', p)
         print(large_jpg_url)
         large_png_url = large_jpg_url.replace('jpg', 'png')
-        downloadImage(downloadpath, f, large_jpg_url, large_png_url)
+        downloadImage(downloadpath, f, large_jpg_url, large_png_url, login_req)
 
 
 # 图片下载
-def downloadImage(downloadpath, filename, jpgurl, pngurl):
+def downloadImage(downloadpath, filename, jpgurl, pngurl, login_req):
     fullpath = os.path.join(downloadpath, filename)
-    jpg = getRequest(req=login, url=jpgurl, headers=headers, stream=True)
-    png = getRequest(req=login, url=pngurl, headers=headers, stream=True)
-
-    with open(fullpath, 'wb') as f:
-        if jpg.status_code == 200:
-            print('图片下载开始%s' % filename)
-            s = datetime.now()
-            f.write(jpg.content)
-            print('图片下载结束 , 耗时 %s 秒 , 文件大小 %s KB' % ((datetime.now() - s).seconds, int(os.path.getsize(fullpath) / 1024)))
-        elif png.status_code == 200:
-            print('图片下载开始%s' % filename)
-            s = datetime.now()
-            f.write(png.content)
-            print('图片下载结束 , 耗时 %s 秒 , 文件大小 %s KB' % ((datetime.now() - s).seconds, int(os.path.getsize(fullpath) / 1024)))
-        else:
-            print('图片下载出错!!!')
+    jpg = login_req.get(url=jpgurl, headers=headers, stream=True)
+    png = login_req.get(url=pngurl, headers=headers, stream=True)
+    if not isImageExist(fullpath):
+        with open(fullpath, 'wb') as f:
+            if jpg.status_code == 200:
+                print('图片下载开始%s' % filename)
+                s = datetime.now()
+                f.write(jpg.content)
+                print('图片下载结束 , 耗时 %s 秒 , 文件大小 %s KB' % ((datetime.now() - s).seconds, int(os.path.getsize(fullpath) / 1024)))
+            elif png.status_code == 200:
+                print('图片下载开始%s' % filename)
+                s = datetime.now()
+                f.write(png.content)
+                print('图片下载结束 , 耗时 %s 秒 , 文件大小 %s KB' % ((datetime.now() - s).seconds, int(os.path.getsize(fullpath) / 1024)))
+            else:
+                print('图片下载出错!!!')
+    else:
+        print('图片已存在!!')
 
 
 # 登录
 login = loginPixiv()
-result = getRequest(req=login, url=CRAWL_Url, headers=headers)
+result = login.get(url=CRAWL_Url, headers=headers)
 
 # 获取我们分析的主要的DOM节点信息
 soup = BeautifulSoup(result.text, "html.parser")
@@ -114,12 +122,13 @@ items = soup.find_all(class_='ranking-item')
 items_length = len(items)
 
 for i in range(10):
-    image_url = 'https://www.pixiv.net' + items[i].find(class_='ranking-image-item').a.get('href')
-    small_jpg_url = items[i].find('img').get('data-src')
+    curr_dom = items[i]
+    image_url = 'https://www.pixiv.net' + curr_dom.find(class_='ranking-image-item').a.get('href')
+    small_jpg_url = curr_dom.find('img').get('data-src')
     headers['Referer'] = image_url
     filename = re.search(re_filename, image_url).group() + '.jpg'
 
-    if isSingleImage(items[i]):
-        singleImageCrawl(small_jpg_url, filename)
+    if isSingleImage(curr_dom):
+        singleImageCrawl(small_jpg_url, filename, login)
     else:
-        manyImageCrawl(small_jpg_url, filename, items[i])
+        manyImageCrawl(small_jpg_url, filename, curr_dom, login)
